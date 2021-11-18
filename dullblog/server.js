d56d5af79331b0b1dac8a6895191d9cd0403b7c4
persr18 <p.s_reed@yahoo.no>
2021-11-18 14:12:14 +0100

    added users

--- dullblog/server.js
+++ dullblog/server.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 8080;
 server.set("port", PORT);
 
 const blogposts = require("./modules/blogposts.js");
+const users = require("./modules/users.js");
 
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
 
 server.use(blogposts);
+server.use(users);
 
 //general error handling ---------------------------
 server.use(function (err, req, res, next) {
@@ -24,4 +26,6 @@ server.use(function (err, req, res, next) {
 server.listen(server.get("port"), function () {
 	console.log("server running", server.get("port"));
 });
+
+const authUtils = require("./modules/auth_utils.js");
 
