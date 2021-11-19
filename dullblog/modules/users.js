b217592476f32d6f19782d5d17504b30ad39ee32
persr18 <p.s_reed@yahoo.no>
2021-11-19 11:36:36 +0100

    add users

--- dullblog/modules/users.js
+++ dullblog/modules/users.js
@@ -23,7 +23,30 @@ router.get("/users", async function(req,res,next){
 
 // create a new user ---------------------
 router.post("/users", async function(req,res,next){
-    res.status(200).send("Hello from POST - /users").end();
+
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+
+    if(cred.username == "" || cred.password == ""){
+        res.status(401).json({error: "No username or password"}).end();
+        return
+    }
+
+    let hash = authUtils.createHash(cred.password);
+
+    try {
+        let data = await db.createUser(cred.username, hash.value, hash.salt);
+
+        if (data.rows.length > 0) {
+            res.status(200).json({msg: "The user was created succefully"}).end();
+        }
+        else {
+            throw "The user couldn't be created";
+        }
+    }
+    catch(err) {
+        next(err);
+    }
 });
 
 // delete a user ---------------------
