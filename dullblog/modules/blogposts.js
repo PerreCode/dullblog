1371d4bd6c68aa36fa73dd82fe970b240769f496
persr18 <p.s_reed@yahoo.no>
2021-11-22 15:39:25 +0100

    final

--- dullblog/modules/blogposts.js
+++ dullblog/modules/blogposts.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const db = require("./db.js");
-const protect = require("./auth");
 const router = express.Router();
+const protect = require("./auth.js");
 
+// endpoints ----------------------------
 router.get("/blogposts", protect, async function(req, res, next) {
-	
-	
-	
-	try{  
+
+	try {
 		let data = await db.getAllBlogPosts();
 		res.status(200).json(data.rows).end();
 	}
-	catch(err){
+	catch (err) {
 		next(err);
-	}
+	}	
 });
 
-router.post("/blogposts", protect, async function(req,res,next){
+router.post("/blogposts", protect, async function(req, res, next) {
+	
 	let updata = req.body;
 	let userid = res.locals.userid;
 
@@ -24,29 +24,32 @@ router.post("/blogposts", protect, async function(req,res,next){
 		let data = await db.createBlogPosts(updata.heading, updata.blogtext, userid);
 
 		if(data.rows.length > 0){
-			res.status(200).json({msg: "the blogpost was created successfully"}).end();
-		}else{
-			throw "the blogpost couldn't be created"
-		}
+			res.status(200).json({msg: "The blogpost was created successfully."}).end();
+			console.log(res.locals);
+		} else {
+			throw "The blogpost couldn't be created";
+		}		
 	}
-	catch(err){
+	catch(err) {
 		next(err);
 	}
 });
 
-router.delete("/blogposts", protect, async function(req, res, next){
+router.delete("/blogposts", protect, async function(req, res, next) {
+
 	let updata = req.body;
 	let userid = res.locals.userid;
+
 	try{
-		let data = await db.deleteBlogPosts(updata.id, userid);
-		if (data.rows.length > 0){
+		let data = await db.deleteBlogPost(updata.id, userid);
+
+		if(data.rows.length > 0){
 			res.status(200).json({msg: "The blogpost was deleted successfully"}).end();
-		}
-		else{
+		} else {
 			throw "The blogpost couldn't be deleted";
-		}
+		}	
 	}
-	catch (err){
+	catch(err){
 		next(err);
 	}
 });
